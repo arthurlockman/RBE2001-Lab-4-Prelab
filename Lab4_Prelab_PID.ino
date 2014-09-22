c7a70bc0dbcf0bee57c187082f9cb9997bc6bbde
Arthur Lockman <hello@rthr.me>
2014-09-22 14:32:29 -0400

    Finished?

--- Lab4_Prelab_PID.ino
+++ Lab4_Prelab_PID.ino
@@ -1,19 +1,25 @@
 #include "Arduino.h"
 #include "TimerOne.h"
 
-float kP = 0.0;
-float kI = 0.0;
-float kD = 0.0;
+int kP = 0.0;
+int kI = 0.0;
+int kD = 0.0;
 int kPIDPeriod = 50;
 
 volatile int input;
 volatile int output;
+
+volatile int m_p;
+volatile int m_i;
+volatile int m_d;
+volatile int m_setpoint;
 
 void setup() 
 {
 	Serial.begin(115200);
 	Timer1.initialize(kPIDPeriod);
 	Timer1.attachInterrupt(updateOutput);
+	m_setpoint = 100;
 }
 
 void loop() 
@@ -21,7 +27,7 @@ void loop()
 	if (Serial.available() > 0)
 	{
 		String gain = Serial.readStringUntil('=');
-		float value = Serial.parseFloat();
+		float value = Serial.parseInt();
 		if (gain == "P" || gain == "p")
 		{
 			kP = value;
@@ -46,5 +52,8 @@ void loop()
 
 void updateOutput()
 {
-	output = 0;
+	int err = m_setpoint - input;
+	m_i += err;
+	output = kP * err + kI * (m_i) + kD * (input - m_d);
+	m_d = input;
 }
